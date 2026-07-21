cd2c4042ee84fd00abe73e3ba138746ea51350bd
Ark-Tek <jqnv02@gmail.com>
2026-07-21 20:58:31 +0200

    Update subnav.php

--- partials/subnav.php
+++ partials/subnav.php
@@ -1,7 +1,6 @@
     <div class="subnav">
       <a class="subnav-home" href="index.php">Mac <span>Music</span> Project</a>
       <nav class="subnav-links" aria-label="Navegación">
-        <a href="quienes-somos.php" data-page="quienes-somos">Quiénes Somos</a>
         <a href="noticias.php" data-page="noticias">Noticias</a>
         <a href="lanzamientos.php" data-page="lanzamientos">Lanzamientos</a>
         <a href="futuros-lanzamientos.php" data-page="futuros-lanzamientos">Futuros Lanzamientos</a>
