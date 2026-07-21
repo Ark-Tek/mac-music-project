7f344c19bbb4a5db1bc3d0d9b02f4fad8c535bb0
Ark-Tek <jqnv02@gmail.com>
2026-07-21 20:59:36 +0200

    Update quienes-somos.php

--- quienes-somos.php
+++ quienes-somos.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Quiénes Somos — Mac Music Project</title>
+<title>Mac Music Project</title>
 <?php include 'partials/head-assets.php'; ?>
 </head>
 <body data-page="quienes-somos">
