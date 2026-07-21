a8d73a67a3182fb9ced286313ef01f69bfff627f
Ark-Tek <jqnv02@gmail.com>
2026-07-21 22:31:30 +0200

    Update quienes-somos.php

--- quienes-somos.php
+++ quienes-somos.php
@@ -7,7 +7,7 @@
 <?php include 'partials/head-assets.php'; ?>
 </head>
 <body data-page="quienes-somos">
-
+<?php include 'partials/cookie-banner.php'; ?>
 <div class="layout">
 
 <?php include 'partials/brand-panel.php'; ?>
