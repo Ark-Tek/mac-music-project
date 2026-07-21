10ae5c052fe8b2986f201398bc9ce0b6f26312b3
Ark-Tek <jqnv02@gmail.com>
2026-07-21 22:21:01 +0200

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,5 @@
-    <footer class="site-footer">Mac Music Project &copy; 2026</footer>
+<footer class="site-footer">
+      Mac Music Project &copy; 2026
+      &middot; <a href="politica-cookies.php">Política de Cookies</a>
+      &middot; <button type="button" class="manage-cookies-link">Configurar cookies</button>
+</footer>
