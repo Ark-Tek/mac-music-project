2d78e2f9a95ef17ec49edc87399303307c2b1050
Ark-Tek <jqnv02@gmail.com>
2026-07-21 22:28:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <?php include 'partials/head-assets.php'; ?>
 </head>
 <body data-page="home">
-
+<?php include 'partials/cookie-banner.php'; ?>
 <div class="layout">
 
 <?php include 'partials/brand-panel.php'; ?>
